a6a9af50ae24b02eeea35c62e2875800dc201bac
kirillbowkin <kirillshishkin77@gmail.com>
2022-12-13 12:41:59 +0300

    2.1 (*) (опциональное) Отображение предыдущих займов клиента

--- src/main/java/com/company/homeworkloans/screen/loan/LoanApproval.java
+++ src/main/java/com/company/homeworkloans/screen/loan/LoanApproval.java
@@ -1,9 +1,13 @@
 package com.company.homeworkloans.screen.loan;
 
 import com.company.homeworkloans.entity.Loan;
+import io.jmix.core.common.util.Preconditions;
 import io.jmix.ui.component.Component;
 import io.jmix.ui.component.Table;
+import io.jmix.ui.model.CollectionLoader;
+import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -12,6 +16,9 @@ import java.time.Period;
 @UiDescriptor("loan-approval.xml")
 @LookupComponent("loansTable")
 public class LoanApproval extends StandardLookup<Loan> {
+    @Autowired
+    private CollectionLoader<Loan> previousLoansDl;
+
     @Install(to = "loansTable.age", subject = "columnGenerator")
     private Component loansTableAgeColumnGenerator(Loan loan) {
         return new Table.PlainTextCell(
@@ -19,4 +26,14 @@ public class LoanApproval extends StandardLookup<Loan> {
                         .getYears())
         );
     }
+
+    @Subscribe(id = "loansDc", target = Target.DATA_CONTAINER)
+    public void onLoansDcItemChange(InstanceContainer.ItemChangeEvent<Loan> event) {
+        Preconditions.checkNotNullArgument(event.getItem().getClient());
+
+        previousLoansDl.setParameter("client", event.getItem().getClient());
+        previousLoansDl.setParameter("id", event.getItem().getId());
+        previousLoansDl.load();
+    }
+
 }
